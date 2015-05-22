27d375cb122b2fc40d0d9b3706606284bdde68ca
ianm3039 <ianm3039@gmail.com>
2015-05-22 13:47:06 -0700

    refactor variable assignments and add main

--- fight_pokemon.py
+++ fight_pokemon.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python
 print("Dueling Pokemon")
 
+# create types for lookup later
+# POKEMON_TYPE[1]='Fire' for example
+POKEMON_TYPES= ['Normal','Fire','Water','Grass','Electric','Ice','Fighting','Poison','Ground',
+                'Flying','Psychic','Bug','Rock','Ghost','Dragon','Dark','Steel','Fairy']
+
+# create table or 'matrix'
+# find value by using MATCHUPS[r][c]
+# where r=row (starts with 0) and c=column (starts with 0)
+MATCHUPS = [ [1,1,1,1,1,1,1,1,1,1,1,1,.5,0,1,1,.5,1], #row 0 or Normal type attacker
+    [1,.5,.5,2,1,2,1,1,1,1,1,2,.5,1,.5,1,2,1],  # row 1 or Fire type
+    [1,2,.5,.5,1,1,1,1,2,1,1,1,2,1,.5,1,1,1]] # row 2 or Water type
+    
+
 #define function to calculate any two combinations
 def calc_effectiveness(attacker_type,defender_type):
     attacker_type_index= POKEMON_TYPES.index(attacker_type)
@@ -10,33 +23,25 @@ def calc_effectiveness(attacker_type,defender_type):
     
     return effectiveness
 
-# create types for lookup later
-# POKEMON_TYPE[1]='Fire' for example
-POKEMON_TYPES= ['Normal','Fire','Water','Grass','Electric','Ice','Fighting','Poison','Ground','Flying','Psychic','Bug','Rock','Ghost','Dragon','Dark','Steel','Fairy']
-
-# create table or 'matrix'
-# find value by using MATCHUPS[r][c]
-# where r=row (starts with 0) and c=column (starts with 0)
-MATCHUPS = [ [1,1,1,1,1,1,1,1,1,1,1,1,.5,0,1,1,.5,1], #row 0 or Normal type attacker
-    [1,.5,.5,2,1,2,1,1,1,1,1,2,.5,1,.5,1,2,1],  # row 1 or Fire type
-    [1,2,.5,.5,1,1,1,1,2,1,1,1,2,1,.5,1,1,1]] # row 2 or Water type
-
-# display example attack effectiveness for Water attacking Fire
-attacker_type_index= POKEMON_TYPES.index('Water')
-defender_type_index=POKEMON_TYPES.index('Fire')
-
-print('Example of Water attacking fire')
-print('attacker index=',attacker_type_index)
-print('defender index=',defender_type_index)
-
-# calculate how effective attacker is against defender
-print('Effectiveness=',MATCHUPS[attacker_type_index][defender_type_index])
-
-# let's try a few different ones
-print('Fire attacks Water:',calc_effectiveness('Fire','Water'))
-print('Water attacks Fire:',calc_effectiveness('Water','Fire'))
-print('Water attacks Water:',calc_effectiveness('Water','Water'))
+def main():
+    
+    # display example attack effectiveness for Water attacking Fire
+    attacker_type_index= POKEMON_TYPES.index('Water')
+    defender_type_index=POKEMON_TYPES.index('Fire')
+    
+    print('Example of Water attacking fire')
+    print('attacker index=',attacker_type_index)
+    print('defender index=',defender_type_index)
+    
+    # calculate how effective attacker is against defender
+    print('Effectiveness=',MATCHUPS[attacker_type_index][defender_type_index])
+    
+    # let's try a few different ones
+    print('Fire attacks Water:',calc_effectiveness('Fire','Water'))
+    print('Water attacks Fire:',calc_effectiveness('Water','Fire'))
+    print('Water attacks Water:',calc_effectiveness('Water','Water'))
 
 
-
+if __name__ == "__main__":
+    main()
     
